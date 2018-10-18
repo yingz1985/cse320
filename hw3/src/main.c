93d2d2020115ad55beca18872dad5a9f260ff23a
Ying Zhang <zhang91@cs.stonybrook.edu>
2018-10-18 18:27:26 -0400

    free method implemented

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -5,19 +5,33 @@
 int main(int argc, char const *argv[]) {
     sf_mem_init();
 
-    double* ptr = sf_malloc(sizeof(double));
+    /*double* ptr = sf_malloc(sizeof(double));
 
     *ptr = 320320320e-320;
 
-    sf_show_heap();
+    //sf_show_heap();
 
     char* array = sf_malloc(4000*sizeof(char));
-    //*array = 'c';
+    *array = 'c';
     //printf("%f\n", *ptr);
-    sf_show_heap();
+    //sf_show_heap();
 
     sf_free(ptr);
+    //sf_show_heap();
     sf_free(array);
+    //sf_show_heap();*/
+
+    void *u = sf_malloc(1); //32
+    /* void *v = */ sf_malloc(40); //48
+    void *w = sf_malloc(152); //160
+    /* void *x = */ sf_malloc(536); //544
+    void *y = sf_malloc(1); // 32
+    /* void *z = */ sf_malloc(2072); //2080
+    sf_malloc(3000);
+
+    sf_free(u);
+    sf_free(w);
+    sf_free(y);
 
     sf_mem_fini();
 
--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -12,13 +12,16 @@
 
 
 sf_epilogue * currentEpilogue;
+sf_prologue * currentPrologueEnd;
 
 sf_free_list_node*  findBlock(size_t size,int findNonEmptyBlock);
+void removeBlockFromFreeList(sf_header* header);
+void insert(size_t difference,sf_free_list_node*newFreeBlock,void * blockAddress);
 
 void setEpilogue(sf_epilogue* e)
 {
     sf_footer footer;
-    sf_block_info Footerinfo = {1,1,0,0,0};
+    sf_block_info Footerinfo = {1,0,0,0,0};
     footer.info = Footerinfo;
 
     sf_epilogue epilogue = {footer};
@@ -43,7 +46,7 @@ void setPrologue(sf_prologue * p)
 
 void addToList(size_t size,sf_free_list_node* node,sf_header * address)
 {
-
+    //size, node to insert in front of , address of header
     sf_header blockHead;
     sf_block_info blockHeadInfo = {0,1,0,size>>4,0};
     blockHead.info = blockHeadInfo;
@@ -65,8 +68,9 @@ void addToList(size_t size,sf_free_list_node* node,sf_header * address)
             blockHead.links.next = sentinelHeader;
             blockHead.links.prev = sentinelHeader;
             *freeBlock = blockHead;
+            sf_footer * foot = (((sf_footer*)(((char*)freeBlock)+size-8)));
+            *foot = blockFoot;
 
-            *(((sf_footer*)((char*)freeBlock+size-8))) = blockFoot;
 
     }
 
@@ -77,6 +81,7 @@ void addToHead(size_t size,sf_free_list_node* node,sf_header * address)
     sf_header header;
     sf_block_info Headerinfo = {0,1,0,size>>4,0};
     header.info = Headerinfo;
+
     sf_footer blockFoot;
     sf_block_info blockFootInfo = {0,1,0,size>>4,0};
     blockFoot.info = blockFootInfo;
@@ -86,42 +91,58 @@ void addToHead(size_t size,sf_free_list_node* node,sf_header * address)
     header.links.prev = &node->head;    //insert at second position right after sentinel node
     (*(sf_header*)(address))= header;
 
-    node->head.links.next = &header;
-    node->head.links.next->links.prev = &header;
-    *(((sf_footer*)((char*)address+size-8))) = blockFoot;
+
+    node->head.links.next->links.prev = address;
+    node->head.links.next = address;
+
+    *(((sf_footer*)(((char*)address)+size-8))) = blockFoot;
+    //sf_show_heap();
 
 }
 
-void coalesce(sf_header* header)
+void coalesce(sf_header* header,int AddBlock)
 {
-    sf_footer * previousBlockFooter = ((sf_footer*)((char*)header-8));
+    sf_footer * previousBlockFooter = ((sf_footer*)(((sf_footer*)header)-1));
     size_t currentBlockSize = header->info.block_size;
-    if(previousBlockFooter->info.allocated == 0)
+    sf_header* headerToInsert = header;
+    int coalesce = 0;
+    //if(previousBlockFooter->info.allocated == 0)
+    if(header->info.prev_allocated==0)
     {
+        coalesce = 1;
+
         //if previous block is not allocated, coalsce
         size_t previousBlockSize = previousBlockFooter->info.block_size<<4;
-        sf_header* previousHeader = ((sf_header*)((char*)header - previousBlockSize));
-        previousHeader->links.prev->links.next = previousHeader->links.next;
-        previousHeader->links.next->links.prev = previousHeader->links.prev;
+        sf_header* previousHeader = ((sf_header*)(((char*)header) - previousBlockSize));
+
+       // previousHeader->links.prev->links.next = previousHeader->links.next;
+       // previousHeader->links.next->links.prev = previousHeader->links.prev;
+        removeBlockFromFreeList(previousHeader);
 
         //need to remove previous header from free list
         //removeBlockFromFreeList(previousHeader);
         previousHeader->info.block_size = previousHeader->info.block_size+ currentBlockSize;
+        headerToInsert = previousHeader;
         //sf_footer* newFooter = (((sf_footer*)((char*)previousHeader+(previousHeader->info.block_size<<4)))-1);
         //newFooter->info = previousHeader->info;
-        sf_free_list_node* node =  findBlock(previousHeader->info.block_size<<4,0);
+        //printf("header%d",headerToInsert->info.block_size);
+    }
 
-        if(node==NULL)//couldn't find a block bigger than it, insert at last position
-            addToList(previousHeader->info.block_size<<4,&sf_free_list_head,previousHeader);
-        else
-            if(node->size > previousHeader->info.block_size<<4)
-                addToList(previousHeader->info.block_size<<4,node,previousHeader);
-            else //if size is exactly the same, insert at head
-                addToHead(previousHeader->info.block_size<<4,node,previousHeader);
+    if(coalesce || AddBlock)
+    {
+        sf_free_list_node* node =  findBlock(headerToInsert->info.block_size<<4,0);
+        insert(headerToInsert->info.block_size<<4,node,headerToInsert);
+        size_t difference = headerToInsert->info.block_size<<4;
 
-
+        sf_header * nextBlockHead = ((sf_header*)((char*)headerToInsert+difference));
+        //if(nextBlockHead!=(sf_header*)currentEpilogue)
+        nextBlockHead->info.prev_allocated = 0;
 
     }
+
+    sf_show_heap();
+
+
 
 
 }
@@ -129,83 +150,37 @@ void initHeap(int initial)
 {
     if(initial)
     {
-        /*sf_header header;
-        sf_block_info Headerinfo = {1,0,0,0,0};
-        header.info = Headerinfo;
 
-        sf_footer Epfooter;
-        sf_block_info EPFooterinfo = {1,0,0,0,0};
-        Epfooter.info = EPFooterinfo;
-
-        sf_prologue prologue = {0,header,Epfooter};
-*/
-        /*sf_footer footer;
-        sf_block_info Footerinfo = {1,1,0,0,0};
-        footer.info = Footerinfo;
-
-        sf_epilogue epilogue = {footer};
-        */
         setEpilogue((((sf_epilogue *)sf_mem_end())-1));
         setPrologue(((sf_prologue *)sf_mem_start()));
         currentEpilogue = (((sf_epilogue *)sf_mem_end())-1);
+        currentPrologueEnd = ((sf_prologue *)sf_mem_start()+1);
         //*((sf_prologue *)sf_mem_start()) = prologue;
 
         size_t pageBlock = (PAGE_SZ - sizeof(sf_prologue)-sizeof(sf_epilogue));
         addToList(pageBlock,&sf_free_list_head,((sf_header*)(    ((sf_prologue *)sf_mem_start())   +1)));
-        /*sf_header blockHead;
-        sf_block_info blockHeadInfo = {0,1,0,pageBlock>>4,0};
-        blockHead.info = blockHeadInfo;
 
-        sf_footer blockFoot;
-        sf_block_info blockFootInfo = {0,1,0,pageBlock>>4,0};
-        blockFoot.info = blockFootInfo;
-
-        //(*(((sf_footer *)sf_mem_end())-2)) = blockFoot;
-        //.info.block_size = pageBlock>>4;
-
-        currentEpilogue = (((sf_epilogue *)sf_mem_end())-1);
-        // *currentEpilogue = epilogue;
-
-        sf_free_list_node* sentinel = sf_add_free_list(pageBlock, sf_free_list_head.next);
-
-        sf_header* freeBlock = ((sf_header*)(    ((sf_prologue *)sf_mem_start())   +1));
-        //sf_free_list_node* sentinel = &sf_free_list_head;
-
-        //&sentinel->next->head;
-
-        if(sentinel)
-        {
-            sf_header* sentinelHeader = &sentinel->head;
-            sentinelHeader->links.next = freeBlock;
-            sentinelHeader->links.prev = freeBlock;
-
-            blockHead.links.next = sentinelHeader;
-            blockHead.links.prev = sentinelHeader;
-            *freeBlock = blockHead;
-
-            *(((sf_footer*)((char*)freeBlock+pageBlock-8))) = blockFoot;
-
-        }*/
-        sf_show_heap();
+        //sf_show_heap();
     }
     else
     {
         setEpilogue((((sf_epilogue *)sf_mem_end())-1)); //set new epilogue
         size_t newPageSize = PAGE_SZ;   //header - epilogue = 0
 
-        sf_block_info oldEpiNewHeader = {0,1,0,newPageSize>>4,0};
-        ((sf_header*)currentEpilogue)->info = oldEpiNewHeader;
+        //sf_block_info oldEpiNewHeader = {0,1,0,newPageSize>>4,0};
+        ((sf_header*)currentEpilogue)->info.block_size = newPageSize>>4;
+        //oldEpiNewHeader;
 
-        coalesce((sf_header*)currentEpilogue);
+        coalesce((sf_header*)currentEpilogue,1);
 
-
+        //sf_show_heap();
         currentEpilogue = (((sf_epilogue *)sf_mem_end())-1);
 
     }
 }
 size_t findBlockSize(size_t size)
 {
-    size_t block = size + sizeof(sf_header);
+    size_t block = size + sizeof(sf_block_info);
     if(block <= 32) return 32;
     else
     {
@@ -244,44 +219,53 @@ sf_free_list_node* findBlock(size_t requestSize,int findNonEmptyBlock)
 
 }
 
-void removeBlockFromFreeList(sf_free_list_node* list,size_t blockSize)
+void removeBlockFromFreeList(sf_header* header)
 {
-    sf_header* header = list->head.links.next;  //first actual header
+    /*sf_header* header = list->head.links.next;  //first actual header
     header->info.allocated = 1;
     header->info.block_size = blockSize>>4;
     if(header->links.prev == header->links.next)
     {
         //if there's only a single block of blockSize
         //remove list from seg list of lists
-        //header->links.prev = &list->head;
-        //header->links.next = &list->head;
+
         list->head.links.next = &list->head;
         list->head.links.prev = &list->head;
+        header->links.prev = NULL;
+        header->links.next = NULL;
 
-        /*if(sf_free_list_head.next == sf_free_list_head.prev)
-        {
-            //if there's only one seg list
-            sf_free_list_head.next = &sf_free_list_head;
-            sf_free_list_head.prev = &sf_free_list_head;
-                //if there's nothing else in the free list, empty free list
-        }
-        else
-        {
-            list->next->prev = list->prev;
-            list->prev->next = list->next;
-
-        }*/
     }
-    else
+    else*/
     {
-        //list->head.links.next = list->head.links.next->links.next;
-        //list->head.links.next->links.next->links.prev = &list->head;
 
         header->links.prev->links.next = header->links.next;
         header->links.next->links.prev = header->links.prev;
-        //list->head = *list->head.links.next;
-        //*header = *header->links.next;
+        header->links.prev = NULL;
+        header->links.next = NULL;
     }
+
+}
+void insert(size_t difference,sf_free_list_node*newFreeBlock,void * blockAddress)
+{
+    if(newFreeBlock==NULL || newFreeBlock->size > difference)
+    {
+        if(newFreeBlock==NULL)
+            addToList(difference,&sf_free_list_head,blockAddress);
+        else
+            addToList(difference,newFreeBlock,blockAddress);
+
+    }
+
+    else if(newFreeBlock->size == difference)
+    {
+        addToHead(difference,newFreeBlock,blockAddress);
+
+    }
+   /* sf_header * nextBlockHead = ((sf_header*)((char*)blockAddress+difference));
+    if(nextBlockHead!=(sf_header*)currentEpilogue)
+        nextBlockHead->info.prev_allocated = 0;
+
+*/
 
 }
 
@@ -295,69 +279,33 @@ void splitBlock(sf_free_list_node* found,size_t block_size)
 
         if(difference >= 32)
         {
-            removeBlockFromFreeList(found,block_size);
+            sf_header* header = found->head.links.next;
+            header->info.allocated = 1;
+            header->info.block_size = block_size>>4;
+            removeBlockFromFreeList(found->head.links.next);
+
+
 
             //if block is big enough to produce another block of >32 size,
             //allocate only necessary size
 
             sf_free_list_node* newFreeBlock = findBlock(difference,0);//an empty list is acceptable
 
-            /*sf_footer blockFooter;
-            sf_block_info i= {0,1,0,difference>>4,0};
-            blockFooter.info = i;
+            insert(difference,newFreeBlock,((sf_header *)(((char*)blockAddress)+block_size)));
+            sf_header* nextHeader = (sf_header*)((char*)header +block_size);
+            nextHeader->info.prev_allocated = 1;
 
-            sf_header insertHead;
-            sf_block_info info = {0,1,0,difference>>4,0};
-            insertHead.info = info;
-*/
-            if(newFreeBlock==NULL || newFreeBlock->size > difference)
-            {
-                //sf_free_list_node *newListNode;
-                if(newFreeBlock==NULL)
-                    addToList(difference,&sf_free_list_head,((sf_header *)(((char*)blockAddress)+block_size)));
-                    //newListNode = sf_add_free_list(difference, sf_free_list_head.next);
-                else
-                    addToList(difference,newFreeBlock,((sf_header *)(((char*)blockAddress)+block_size)));
-                    //newListNode = sf_add_free_list(difference, newFreeBlock);
-
-                /*sf_header* freeBlock = ((sf_header *)(((char*)blockAddress)+block_size));
-
-                sf_header* sentinelHeader = &newListNode->head;
-                sentinelHeader->links.next = freeBlock;
-                sentinelHeader->links.prev = freeBlock;
-
-                insertHead.links.next = sentinelHeader;
-                insertHead.links.prev = sentinelHeader;
-                *freeBlock = insertHead;
-
-
-                *(sf_footer*)(((char*)blockAddress)+found->size-8) = blockFooter;
-
-*/
-
-            }
-
-            else if(newFreeBlock->size == difference)
-            {
-                //if found a block exactly the size, insert into head
-                addToHead(difference,newFreeBlock,(sf_header*)((char*)blockAddress+block_size));
-                /*sf_header header;
-                sf_block_info Headerinfo = {0,1,0,difference>>4,0};
-                header.info = Headerinfo;
-                header.links.next = &newFreeBlock->head;
-                header.links.prev = newFreeBlock->head.links.prev;
-                (*(sf_header*)((char*)blockAddress+found->size))= header;
-
-                newFreeBlock->head.links.prev->links.next = &header;
-                newFreeBlock->head = header;*/
-
-            }
 
         }
         else
         {
             //else, use the whole block
-            removeBlockFromFreeList(found,found->size);
+            sf_header* header = found->head.links.next;
+            header->info.allocated = 1;
+            header->info.block_size = found->size>>4;
+            removeBlockFromFreeList(found->head.links.next);
+            sf_header* nextHeader = (sf_header*)((char*)header +(header->info.block_size<<4));
+            nextHeader->info.prev_allocated = 1;
 
         }
 
@@ -383,93 +331,8 @@ void *sf_malloc(size_t size) {
         //size_t difference = found->size - block_size;
         found->head.links.next->info.requested_size = size;
         splitBlock(found,block_size);
-        return blockAddress+1;
+        return ((sf_block_info*)blockAddress)+1;
 
-       /* if(difference >= 32)
-        {
-            removeBlockFromFreeList(found,block_size);
-
-            //if block is big enough to produce another block of >32 size,
-            //allocate only necessary size
-
-            sf_free_list_node* newFreeBlock = findBlock(difference,0);//an empty list is acceptable
-
-            sf_footer blockFooter;
-            sf_block_info i= {0,1,0,difference>>4,0};
-            blockFooter.info = i;
-
-            sf_header insertHead;
-            sf_block_info info = {0,1,0,difference>>4,0};
-            insertHead.info = info;
-
-            if(newFreeBlock==NULL || newFreeBlock->size > difference)
-            {
-
-                sf_free_list_node * newListNode = sf_add_free_list(difference, sf_free_list_head.next);
-
-                sf_header* freeBlock = ((sf_header *)(((char*)blockAddress)+block_size));
-
-                sf_header* sentinelHeader = &newListNode->head;
-                sentinelHeader->links.next = freeBlock;
-                sentinelHeader->links.prev = freeBlock;
-
-                insertHead.links.next = sentinelHeader;
-                insertHead.links.prev = sentinelHeader;
-                *freeBlock = insertHead;
-
-
-                *(sf_footer*)(((char*)blockAddress)+found->size-8) = blockFooter;
-
-
-
-
-            }
-            //else if(newFreeBlock->size > difference)
-            {
-                //if didnt find a block of right size to insert into
-                sf_free_list_node * newListNode = sf_add_free_list(difference, newFreeBlock);
-
-
-
-
-
-                *((sf_header*)((char*)blockAddress+block_size)) = insertHead;
-
-
-                (*(sf_header*)(blockAddress+block_size)).info.prev_allocated = 1;
-
-                *(sf_footer*)(((char*)blockAddress)+found->size-8) = blockFooter;
-
-
-
-            }
-            else if(newFreeBlock->size == difference)
-            {
-                //if found a block exactly the size, insert into head
-                sf_header header;
-                sf_block_info Headerinfo = {1,0,0,0,0};
-                header.info = Headerinfo;
-                header.links.next = &newFreeBlock->head;
-                header.links.prev = newFreeBlock->head.links.prev;
-                (*(sf_header*)((char*)blockAddress+found->size))= header;
-
-                newFreeBlock->head.links.prev->links.next =&header;
-                newFreeBlock->head = header;
-
-            }
-
-            return blockAddress+1;
-
-
-        }
-        else
-        {
-            //else, use the whole block
-            removeBlockFromFreeList(found,found->size);
-            return blockAddress+1;
-
-        }
-*/
     }
     else
     {
@@ -481,8 +344,6 @@ void *sf_malloc(size_t size) {
             {
                 initHeap(0);
                 found = findBlock(block_size,1);
-
-
             }
             else
             {
@@ -490,23 +351,61 @@ void *sf_malloc(size_t size) {
                 return NULL;
             }
 
-
         }
 
         sf_header* blockAddress = found->head.links.next;
         found->head.links.next->info.requested_size = size;
         splitBlock(found,block_size);
-        return blockAddress+1;
-
+        return ((sf_block_info*)blockAddress)+1;
 
     }
-
 
 
     return NULL;
 }
 
 void sf_free(void *pp) {
+    //pp is the first byte of payload
+    if(pp==NULL) abort();
+    sf_header* header = (sf_header*)((sf_block_info*)pp-1);
+    size_t block_size = header->info.block_size<<4;
+    if((void*)header < (void*)currentPrologueEnd) abort();  //if header appears before the end of prologue
+    if(header->info.allocated==0) abort();
+    if(block_size%16!=0  || block_size<32 ) abort();
+    if(header->info.requested_size + sizeof(sf_block_info) > block_size) abort();
+
+    sf_footer* previousFoot = ((sf_footer*)pp-2);
+    sf_header* previousHead = (sf_header*)(((char*)pp)-(previousFoot->info.block_size<<4));
+
+    if(header->info.prev_allocated==0)
+    {
+        if(previousFoot->info.allocated == 1 || previousHead->info.allocated == 1)
+            abort();
+    }
+    sf_header* nextBlockHeader = ((sf_header*)(((char*)header)+(header->info.block_size<<4)));
+    //(((sf_header*)(((char*)header)+size-8)))
+    //sf_show_heap();
+    coalesce(header,1);
+
+    sf_show_heap();
+
+    if(nextBlockHeader->info.allocated==0)
+    {
+            removeBlockFromFreeList(nextBlockHeader);
+            //header->links.prev->links.next = header->links.next;
+            //header->links.next->links.prev = header->links.prev;
+            coalesce(nextBlockHeader,1);
+            //nextBlockHeader->info.prev_allocated = 0;
+
+    }
+
+   // sf_show_heap();
+    //see if block after can be coalesced as well
+
+
+
+
+
     return;
 }
 
